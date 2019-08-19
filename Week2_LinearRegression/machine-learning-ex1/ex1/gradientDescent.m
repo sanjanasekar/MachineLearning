d924113d363f10ccb1f01f1abe49237850240f33
Sanjana Sekar <sanjanasekar29@gmail.com>
2019-08-18 17:54:52 -0700

    [ML]: Linear Regression with One Variable using Gradient Descent Algorithm

--- Week2_LinearRegression/machine-learning-ex1/ex1/gradientDescent.m
+++ Week2_LinearRegression/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,18 +16,28 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    % if theta_j is the parameter for feature x_j 
+    %then the computation is done using the following expression
+    %theta_j := theta_j - alpha * (1/m) [sum(h(x_i) - y_i)] * x_j
+    theta_1 = theta(1);
+    theta_2 = theta(2);
+    h_X = X * theta;
+    hX_y_DIFF = h_X - y; 
+    theta_1 = theta_1 - (alpha * (1/m) * (sum(hX_y_DIFF.* X(:,1))));
+    theta_2 = theta_2 - (alpha * (1/m) * (sum(hX_y_DIFF .* X(:,2))));
 
-
-
-
-
+    %Update theta_1 and theta_2 simultaneously
+    theta = [ theta_1; theta_2];
 
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    #fprintf('Iteration #%d completed \n', iter);
+    #fprintf('With theta = %f\n Cost computed = %f\n', theta, J_history(iter));
 
 end
+
 
 end
